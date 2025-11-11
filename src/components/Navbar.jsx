e4d7719c959807b7699feec9b02f3db7a9abbbe7
jubayer-bd <mjh84349@gmail.com>
2025-11-11 09:49:37 +0600

    nav ber fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -176,13 +176,15 @@ const Navbar = () => {
               tabIndex={0}
               className="menu menu-sm dropdown-content mt-3 z-1 p-2 shadow bg-base-100 rounded-box w-52"
             >
-              <Link
-                to="/register"
-                className="btn btn-sm bg-primary text-white hover:bg-primary/90  flex items-center gap-1"
-              >
-                <HiOutlineLogin />
-                <span className="">Register</span>
-              </Link>
+              {!user && (
+                <Link
+                  to="/register"
+                  className="btn btn-sm bg-primary text-white hover:bg-primary/90  flex items-center gap-1"
+                >
+                  <HiOutlineLogin />
+                  <span className="">Register</span>
+                </Link>
+              )}
               {navLinks}
               <button
                 onClick={toggleTheme}
